814d57b01fe948ab8f81532e3d74bd80b0984605
Erick Sorto <SortoErick33@gmail.com>
2022-11-16 15:38:26 -0500

    Implemeneted methods

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,81 @@
+import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main implements MoviesProject {
 
-        Map<String, Details> moviesMap = new Repository().createMusicArrayList();
+    private static Map<String,Details> moviesMap;
+
+
+    public static void main(String[] args) throws IOException {
+        Main ex = new Main();
+
+
         Repository repository = new Repository();
         moviesMap = repository.createMusicArrayList();
 
         for (Map.Entry<String, Details> entry : moviesMap.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue().returnAllDetails());
         }
+
+        System.out.println(ex.find("Bridget Jones's Diary"));
+        System.out.println(ex.add("chicken", new String[]{"chicken", "chicken", "chicken"}));
+        System.out.println(ex.getLoadFactor());
+        System.out.println(ex.find("chicken"));
+
+    }
+
+    @Override
+    public String find(String movie) {
+        return moviesMap.containsKey(movie) ?  movie + " " + moviesMap.get(movie).returnAllDetails() : "Movie not found";
+    }
+
+    @Override
+    public boolean add(String movie, String[] entry) {
+
+        return moviesMap.putIfAbsent(movie, new Details(entry[0], entry[1], entry[2])) == null;
+    }
+
+    @Override
+    public boolean delete(String movie) {
+        return moviesMap.remove(movie) != null;
+    }
+
+    @Override
+    public void printHT() {
+        for (Map.Entry<String, Details> entry : moviesMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue().returnAllDetails());
+        }
+    }
+
+    @Override
+    public double getLoadFactor() {
+        return moviesMap.size() / 1000.0;
+    }
+
+    @Override
+    public double getMaxLoadFactor() {
+        return moviesMap.size() / moviesMap.keySet().size();
+    }
+
+    @Override
+    public int count() {
+        return moviesMap.size();
+    }
+
+    @Override
+    public void who() {
+
+    }
+
+    @Override
+    public void help() {
+
+    }
+
+    @Override
+    public void exit() {
 
     }
 }
