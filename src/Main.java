7b388da1b739b4339ca8d9f972987a6c53db172b
Erick Sorto <SortoErick33@gmail.com>
2022-11-20 12:17:28 -0500

    Added options function

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main implements MoviesProject {
 
@@ -24,6 +25,11 @@ public class Main implements MoviesProject {
         System.out.println(ex.getLoadFactor());
         System.out.println(ex.find("chicken"));
 
+
+        ex.printOptions();
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        ex.optionChosen(option);
     }
 
     public void printOptions() {
@@ -37,6 +43,57 @@ public class Main implements MoviesProject {
         System.out.println("8. Who");
         System.out.println("9. Help");
         System.out.println("10. Exit");
+
+    }
+
+    public void optionChosen(int option){
+        switch (option){
+            case 1:
+                System.out.println("Enter the name of the movie you want to find");
+                Scanner scanner = new Scanner(System.in);
+                String movieName = scanner.nextLine();
+                System.out.println(find(movieName));
+                break;
+            case 2:
+                System.out.println("Enter the name of the movie you want to add");
+                Scanner scanner1 = new Scanner(System.in);
+                String movieName1 = scanner1.nextLine();
+                System.out.println("Enter the details of the movie you want to add");
+                Scanner scanner2 = new Scanner(System.in);
+                String movieDetails = scanner2.nextLine();
+                String[] movieDetailsArray = movieDetails.split(",");
+                System.out.println(add(movieName1,movieDetailsArray));
+                break;
+            case 3:
+                System.out.println("Enter the name of the movie you want to remove");
+                Scanner scanner3 = new Scanner(System.in);
+                String movieName2 = scanner3.nextLine();
+                System.out.println(delete(movieName2));
+                break;
+            case 4:
+               printHT();
+                break;
+            case 5:
+                System.out.println(getLoadFactor());
+                break;
+            case 6:
+                System.out.println(getMaxLoadFactor());
+                break;
+            case 7:
+                System.out.println(count());
+                break;
+            case 8:
+                who();
+                break;
+            case 9:
+                help();
+                break;
+            case 10:
+                exit();
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
     }
 
     @Override
